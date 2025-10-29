89c7d38b2bf383200c34dfae8eb0e1249fd782c9
Davanesh <davaneshsaminathan335@gmail.com>
2025-10-29 18:44:22 +0530

    user inputs

--- main.go
+++ main.go
@@ -3,6 +3,28 @@ package main
 import "fmt"
 
 func main() {
+	var totalTickets int = 50
+	var userFirstName string
+	var userLastName string
+	var userEmail string
+	var userTickets int
+
 	fmt.Println("Welcome to booking application")
 	fmt.Println("You can book tickets here")
+	fmt.Printf("Total tickets available: %v\n", totalTickets)
+
+	fmt.Print("Enter your first name: ")
+	fmt.Scan(&userFirstName)
+	fmt.Print("Enter your last name: ")
+	fmt.Scan(&userLastName)
+	var userName string = userFirstName + " " + userLastName 
+
+	fmt.Print("Enter your email ID name: ")
+	fmt.Scan(&userEmail)
+	
+	fmt.Print("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	totalTickets -= userTickets
+	fmt.Printf("Thanks %v, Your ticket has been sent to %v :)", userName, userEmail)
 }
